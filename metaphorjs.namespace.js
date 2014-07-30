ff33254cc990febb18a75d26f775a897479b90e5
kuindji <kuindji@gmail.com>
2014-07-30 13:17:29 +0400

    initial

---
+++ metaphorjs.namespace.js
@@ -0,0 +1,135 @@
+(function(){
+
+    "use strict";
+
+    /**
+     * @namespace MetaphorJs
+     */
+
+    var root        = typeof window != "undefined" ? window : global,
+        cache       = {};
+
+    var parseNs     = function(ns) {
+
+        var tmp     = ns.split("."),
+            i,
+            last    = tmp.pop(),
+            parent  = tmp.join("."),
+            len     = tmp.length,
+            name,
+            current = root;
+
+        if (cache[parent]) {
+            return [cache[parent], last];
+        }
+
+        for (i = 0; i < len; i++) {
+
+            name    = tmp[i];
+
+            if (!current[name]) {
+                current[name]   = {};
+            }
+
+            current = current[name];
+        }
+
+        return [current, last];
+    };
+
+    /**
+     * Get namespace/cache object
+     * @function MetaphorJs.ns.get
+     * @param {string} ns
+     * @param {bool} cacheOnly
+     * @returns {object} constructor
+     */
+    var get       = function(ns, cacheOnly) {
+
+        if (cache[ns] || cacheOnly) {
+            return cache[ns];
+        }
+
+        var tmp     = ns.split("."),
+            i,
+            len     = tmp.length,
+            name,
+            current = root;
+
+        for (i = 0; i < len; i++) {
+
+            name    = tmp[i];
+
+            if (!current[name]) {
+                return null;
+            }
+
+            current = current[name];
+        }
+
+        return current;
+    };
+
+    /**
+     * Register class constructor
+     * @function MetaphorJs.ns.register
+     * @param {string} ns
+     * @param {*} fn
+     */
+    var register    = function(ns, fn) {
+
+        var parse   = parseNs(ns),
+            parent  = parse[0],
+            name    = parse[1];
+
+        parent[name]    = fn;
+        cache[ns]       = fn;
+
+        return fn;
+    };
+
+    /**
+     * Class exists
+     * @function MetaphorJs.ns.exists
+     * @param {string} ns
+     * @returns boolean
+     */
+    var exists      = function(ns) {
+        return cache[ns] ? true : false;
+    };
+
+    /**
+     * Add constructor to cache
+     * @function MetaphorJs.ns.add
+     * @param {string} ns
+     * @param {function} c
+     */
+    var add = function(ns, c) {
+        cache[ns] = c;
+        return c;
+    };
+
+    register("MetaphorJs.ns", {
+        register:   register,
+        exists:     exists,
+        get:        get,
+        add:        add,
+        /**
+         * Remove constructor from cache
+         * @function MetaphorJs.ns.remove
+         * @param {string} ns
+         */
+        remove:     function(ns) {
+            delete cache[ns];
+        }
+    });
+
+    MetaphorJs.r = register;
+    MetaphorJs.g = get;
+    MetaphorJs.add = add;
+
+    if (typeof module != "undefined") {
+        module.exports = MetaphorJs.ns;
+    }
+
+}());
