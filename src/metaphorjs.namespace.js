7e8180300b7cf3c496e0e68367813526fd6eac1e
kuindji <kuindji@gmail.com>
2014-09-26 16:51:17 +0400

    ...

--- src/metaphorjs.namespace.js
+++ src/metaphorjs.namespace.js
@@ -3,58 +3,115 @@ var undf = require("../../metaphorjs/src/var/undf.js"),
     strUndef = require("../../metaphorjs/src/var/strUndef.js"),
     isObject    = require("../../metaphorjs/src/func/isObject.js");
 
-"use strict";
+module.exports = function(){
 
-/**
- * @param {Object} root optional; usually window or global
- * @param {String} rootName optional. If you want custom object to be root and
- * this object itself if the first level of namespace:<br>
- * <pre><code class="language-javascript">
- * var ns = MetaphorJs.lib.Namespace(window);
- * ns.register("My.Test", something); // -> window.My.Test
- * var privateNs = {};
- * var ns = new MetaphorJs.lib.Namespace(privateNs, "privateNs");
- * ns.register("privateNs.Test", something); // -> privateNs.Test
- * </code></pre>
- * @constructor
- */
-var Namespace   = function(root, rootName) {
+    /**
+     * @param {Object} root optional; usually window or global
+     * @param {String} rootName optional. If you want custom object to be root and
+     * this object itself if the first level of namespace:<br>
+     * <pre><code class="language-javascript">
+     * var ns = MetaphorJs.lib.Namespace(window);
+     * ns.register("My.Test", something); // -> window.My.Test
+     * var privateNs = {};
+     * var ns = new MetaphorJs.lib.Namespace(privateNs, "privateNs");
+     * ns.register("privateNs.Test", something); // -> privateNs.Test
+     * </code></pre>
+     * @constructor
+     */
+    var Namespace   = function(root, rootName) {
 
-    var cache   = {},
-        self    = this;
+        var cache   = {},
+            self    = this;
 
-    if (!root) {
-        if (typeof global != strUndef) {
-            root    = global;
-        }
-        else {
-            root    = window;
-        }
-    }
-
-    var normalize   = function(ns) {
-        if (ns && rootName && ns.indexOf(rootName) !== 0) {
-            return rootName + "." + ns;
-        }
-        return ns;
-    };
-
-    var parseNs     = function(ns) {
-
-        var tmp     = ns.split("."),
-            i,
-            last    = tmp.pop(),
-            parent  = tmp.join("."),
-            len     = tmp.length,
-            name,
-            current = root;
-
-
-        if (cache[parent]) {
-            return [cache[parent], last, ns];
+        if (!root) {
+            if (typeof global != strUndef) {
+                root    = global;
+            }
+            else {
+                root    = window;
+            }
         }
 
-        if (len > 0) {
+        var normalize   = function(ns) {
+            if (ns && rootName && ns.indexOf(rootName) !== 0) {
+                return rootName + "." + ns;
+            }
+            return ns;
+        };
+
+        var parseNs     = function(ns) {
+
+            var tmp     = ns.split("."),
+                i,
+                last    = tmp.pop(),
+                parent  = tmp.join("."),
+                len     = tmp.length,
+                name,
+                current = root;
+
+
+            if (cache[parent]) {
+                return [cache[parent], last, ns];
+            }
+
+            if (len > 0) {
+                for (i = 0; i < len; i++) {
+
+                    name    = tmp[i];
+
+                    if (rootName && i == 0) {
+                        if (name == rootName) {
+                            current = root;
+                            continue;
+                        }
+                        else {
+                            ns = rootName + "." + ns;
+                        }
+                    }
+
+                    if (current[name] === undf) {
+                        current[name]   = {};
+                    }
+
+                    current = current[name];
+                }
+            }
+            else {
+                if (rootName) {
+                    ns = rootName + "." + ns;
+                }
+            }
+
+            return [current, last, ns];
+        };
+
+        /**
+         * Get namespace/cache object
+         * @function MetaphorJs.ns.get
+         * @param {string} ns
+         * @param {bool} cacheOnly
+         * @returns {object} constructor
+         */
+        var get       = function(ns, cacheOnly) {
+
+            if (cache[ns] !== undf) {
+                return cache[ns];
+            }
+
+            if (rootName && cache[rootName + "." + ns] !== undf) {
+                return cache[rootName + "." + ns];
+            }
+
+            if (cacheOnly) {
+                return undf;
+            }
+
+            var tmp     = ns.split("."),
+                i,
+                len     = tmp.length,
+                name,
+                current = root;
+
             for (i = 0; i < len; i++) {
 
                 name    = tmp[i];
@@ -64,144 +121,97 @@ var Namespace   = function(root, rootName) {
                         current = root;
                         continue;
                     }
-                    else {
-                        ns = rootName + "." + ns;
-                    }
                 }
 
                 if (current[name] === undf) {
-                    current[name]   = {};
+                    return undf;
                 }
 
                 current = current[name];
             }
-        }
-        else {
-            if (rootName) {
+
+            if (current) {
+                cache[ns] = current;
+            }
+
+            return current;
+        };
+
+        /**
+         * Register class constructor
+         * @function MetaphorJs.ns.register
+         * @param {string} ns
+         * @param {*} value
+         */
+        var register    = function(ns, value) {
+
+            var parse   = parseNs(ns),
+                parent  = parse[0],
+                name    = parse[1];
+
+            if (isObject(parent) && parent[name] === undf) {
+
+                parent[name]        = value;
+                cache[parse[2]]     = value;
+            }
+
+            return value;
+        };
+
+        /**
+         * Class exists
+         * @function MetaphorJs.ns.exists
+         * @param {string} ns
+         * @returns boolean
+         */
+        var exists      = function(ns) {
+            return cache[ns] !== undf;
+        };
+
+        /**
+         * Add constructor to cache
+         * @function MetaphorJs.ns.add
+         * @param {string} ns
+         * @param {*} value
+         */
+        var add = function(ns, value) {
+            if (rootName && ns.indexOf(rootName) !== 0) {
                 ns = rootName + "." + ns;
             }
-        }
-
-        return [current, last, ns];
-    };
-
-    /**
-     * Get namespace/cache object
-     * @function MetaphorJs.ns.get
-     * @param {string} ns
-     * @param {bool} cacheOnly
-     * @returns {object} constructor
-     */
-    var get       = function(ns, cacheOnly) {
-
-        if (cache[ns] !== undf) {
-            return cache[ns];
-        }
-
-        if (rootName && cache[rootName + "." + ns] !== undf) {
-            return cache[rootName + "." + ns];
-        }
-
-        if (cacheOnly) {
-            return undf;
-        }
-
-        var tmp     = ns.split("."),
-            i,
-            len     = tmp.length,
-            name,
-            current = root;
-
-        for (i = 0; i < len; i++) {
-
-            name    = tmp[i];
-
-            if (rootName && i == 0) {
-                if (name == rootName) {
-                    current = root;
-                    continue;
-                }
+            if (cache[ns] === undf) {
+                cache[ns] = value;
             }
+            return value;
+        };
 
-            if (current[name] === undf) {
-                return undf;
-            }
+        var remove = function(ns) {
+            delete cache[ns];
+        };
 
-            current = current[name];
-        }
-
-        if (current) {
-            cache[ns] = current;
-        }
-
-        return current;
+        self.register   = register;
+        self.exists     = exists;
+        self.get        = get;
+        self.add        = add;
+        self.remove     = remove;
+        self.normalize  = normalize;
     };
 
-    /**
-     * Register class constructor
-     * @function MetaphorJs.ns.register
-     * @param {string} ns
-     * @param {*} value
-     */
-    var register    = function(ns, value) {
+    Namespace.prototype.register = null;
+    Namespace.prototype.exists = null;
+    Namespace.prototype.get = null;
+    Namespace.prototype.add = null;
+    Namespace.prototype.remove = null;
+    Namespace.prototype.normalize = null;
 
-        var parse   = parseNs(ns),
-            parent  = parse[0],
-            name    = parse[1];
+    var globalNs;
 
-        if (isObject(parent) && parent[name] === undf) {
-
-            parent[name]        = value;
-            cache[parse[2]]     = value;
+    Namespace.global = function() {
+        if (!globalNs) {
+            globalNs = new Namespace;
         }
-
-        return value;
+        return globalNs;
     };
 
-    /**
-     * Class exists
-     * @function MetaphorJs.ns.exists
-     * @param {string} ns
-     * @returns boolean
-     */
-    var exists      = function(ns) {
-        return cache[ns] !== undf;
-    };
+    return Namespace;
 
-    /**
-     * Add constructor to cache
-     * @function MetaphorJs.ns.add
-     * @param {string} ns
-     * @param {*} value
-     */
-    var add = function(ns, value) {
-        if (rootName && ns.indexOf(rootName) !== 0) {
-            ns = rootName + "." + ns;
-        }
-        if (cache[ns] === undf) {
-            cache[ns] = value;
-        }
-        return value;
-    };
-
-    var remove = function(ns) {
-        delete cache[ns];
-    };
-
-    self.register   = register;
-    self.exists     = exists;
-    self.get        = get;
-    self.add        = add;
-    self.remove     = remove;
-    self.normalize  = normalize;
-};
-
-Namespace.prototype.register = null;
-Namespace.prototype.exists = null;
-Namespace.prototype.get = null;
-Namespace.prototype.add = null;
-Namespace.prototype.remove = null;
-Namespace.prototype.normalize = null;
-
-
-module.exports = Namespace;
+}();
